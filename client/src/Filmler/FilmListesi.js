050d2185539face7e7e25e8a840b9785cfab19d4
ayşe <ayse.efsin@yandex.com>
2023-11-01 07:37:58 +0300

    end

--- client/src/Filmler/FilmListesi.js
+++ client/src/Filmler/FilmListesi.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 export default function FilmListesi(props) {
   return (
     <div className="movie-list">
       {props.movies.map((movie) => (
-        <FilmDetayları key={movie.id} movie={movie} />
+        <FilmDetaylari key={movie.id} movie={movie} />
       ))}
     </div>
   );
 }
 
-function FilmDetayları(props) {
+function FilmDetaylari(props) {
   const { title, director, metascore, id } = props.movie;
 
   return (
-    <Link to={`/filmler${id}`}>
+    <Link to={`/filmler/${id}`}>
       <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
