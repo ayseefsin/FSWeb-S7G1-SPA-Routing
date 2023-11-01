b1992e23708c8ae961caf6a85e37c32c740b6079
ayşe <ayse.efsin@yandex.com>
2023-11-01 04:45:55 +0300

    bir kısmı yapıldı

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import KaydedilenlerListesi from './Filmler/KaydedilenlerListesi';
+import KaydedilenlerListesi from "./Filmler/KaydedilenlerListesi";
+import Film from "./Filmler/Film";
+import FilmListesi from "./Filmler/FilmListesi";
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const FilmleriAl = () => {
       axios
-        .get('http://localhost:5001/api/filmler') // Burayı Postman'le çalışın
-        .then(response => {
+        .get("http://localhost:5001/api/filmler") // Burayı Postman'le çalışın
+        .then((response) => {
           // Bu kısmı log statementlarıyla çalışın
+          console.log(response.data);
           // ve burdan gelen response'u 'movieList' e aktarın
+          setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Sunucu Hatası', error);
+        .catch((error) => {
+          console.error("Sunucu Hatası", error);
         });
-    }
+    };
     FilmleriAl();
   }, []);
 
-  const KaydedilenlerListesineEkle = id => {
+  const KaydedilenlerListesineEkle = (id) => {
     // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
   };
 
   return (
     <div>
-      <KaydedilenlerListesi list={[ /* Burası esnek */]} />
+      <KaydedilenlerListesi
+        list={
+          [
+            /* Burası esnek */
+          ]
+        }
+      />
 
-      <div>Bu Div'i kendi Routelarınızla değiştirin</div>
+      <Switch>
+        <Route path="/filmler/:id">
+          <Film />
+        </Route>
+        <Route path="/" exact>
+          <FilmListesi movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 }
